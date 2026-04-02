d2ee53318e4a7505cdfe0b4a8705fc3effad155a
ti <tiwang.cs@gmail.com>
2026-04-02 17:20:35 +0200

    Remove CUDA_VISIBLE_DEVICES setting from train.sh for both stages

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ exp_name_stage1=primaStage1
 exp_name_stage2=primaStage2
 experiment1=primaStage1
 experiment2=primaStage2
-HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=1 python train.py exp_name=$exp_name_stage1 experiment=$experiment1 trainer=gpu launcher=local
+HYDRA_FULL_ERROR=1 python train.py exp_name=$exp_name_stage1 experiment=$experiment1 trainer=gpu launcher=local
 cp -r ./logs/train/runs/$exp_name_stage1 ./logs/train/runs/$exp_name_stage2
-HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=1 python train.py exp_name=$exp_name_stage2 experiment=$experiment2 trainer=gpu launcher=local
+HYDRA_FULL_ERROR=1 python train.py exp_name=$exp_name_stage2 experiment=$experiment2 trainer=gpu launcher=local
