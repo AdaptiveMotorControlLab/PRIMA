39bde99650733cb12021052e3f10c0c9b418aef4
ti <ti.wang@epfl.ch>
2026-05-21 15:12:18 +0200

    fix example checkpoint path in demo.sh

--- demo.sh
+++ demo.sh
@@ -3,7 +3,7 @@
 #
 # If this local file is missing, it will be downloaded from the PRIMA Hugging Face repo.
 # To use another local checkpoint instead, update this path.
-# For example: checkpoint='data/PRIMAS3/checkpoints/s3ckpt.ckpt'
+# For example: checkpoint='data/PRIMAS3/checkpoints/s3ckpt_inference.ckpt'
 checkpoint='data/PRIMAS1/checkpoints/s1ckpt_inference.ckpt'
 
 python demo.py \
