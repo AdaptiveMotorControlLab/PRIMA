9bee30d67258097729c0dfd4588c69e43a75795a
ti <ti.wang@epfl.ch>
2026-05-21 15:12:00 +0200

    fix comment for example checkpoint path in demo_tta.sh

--- demo_tta.sh
+++ demo_tta.sh
@@ -4,7 +4,7 @@
 #
 # This standard path is auto-downloaded from the PRIMA Hugging Face repo if missing.
 # To use another local checkpoint instead, update this path.
-# For example: checkpoint='data/PRIMAS3/checkpoints/s3ckpt.ckpt'
+# For example: checkpoint='data/PRIMAS3/checkpoints/s3ckpt_inference.ckpt'
 checkpoint='data/PRIMAS1/checkpoints/s1ckpt_inference.ckpt'
 
 python3 demo_tta.py \
